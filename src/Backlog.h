a757d145ed8efc00706378f98f5c7930b2267c11
Max Nerius <nermax03@gmail.com>
2022-11-23 19:13:37 +0100

    sketch out project structure

---
+++ src/Backlog.h
@@ -0,0 +1,11 @@
+//
+// Created by thulis on 23.11.22.
+//
+
+#ifndef IRCCLIENT_BACKLOG_H
+#define IRCCLIENT_BACKLOG_H
+
+class Backlog {
+};
+
+#endif // IRCCLIENT_BACKLOG_H
---
+++ src/Session.h
@@ -0,0 +1,11 @@
+//
+// Created by thulis on 23.11.22.
+//
+
+#ifndef IRCCLIENT_SESSION_H
+#define IRCCLIENT_SESSION_H
+
+class Session {
+};
+
+#endif // IRCCLIENT_SESSION_H
